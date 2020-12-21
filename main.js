2162d5011da12cf59fb90945b7fd354716c49db9
avelica-gif <73679187+avelica-gif@users.noreply.github.com>
2020-12-20 19:12:59 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+// store the value of the input
+let city = $("#searchTerm").val();
+// store api key
+const apiKey = "&appid=afaa8eea1769b4359fd8e07b2efcefbd";
+
+let date = new Date();
+
+$("#searchTerm").keypress(function (event) {
+
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        $("#searchBtn").click();
+    }
+});
+
+$("#searchBtn").on("click", function () {
+
+    $('#forecastH5').addClass('show');
+
+    // get the value of the input from user
+    city = $("#searchTerm").val();
+
+    // clear input box
+    $("#searchTerm").val("");
+
+    // full url to call api
+    const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            console.log(response)
+
+            console.log(response.name)
+            console.log(response.weather[0].icon)
+
+            let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            console.log(Math.floor(tempF))
+
+            console.log(response.main.humidity)
+
+            console.log(response.wind.speed)
+
+            getCurrentConditions(response);
+            getCurrentForecast(response);
+            makeList();
+
+        })
+});
+
+function makeList() {
+    let listItem = $("<li>").addClass("list-group-item").text(city);
+    $(".list").append(listItem);
+}
+
+function getCurrentConditions(response) {
+
+    // get the temperature and convert to fahrenheit 
+    let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    tempF = Math.floor(tempF);
+
+    $('#currentCity').empty();
+
+    // get and set the content 
+    const card = $("<div>").addClass("card");
+    const cardBody = $("<div>").addClass("card-body");
+    const city = $("<h4>").addClass("card-title").text(response.name);
+    const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+    const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+    const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+    const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+    const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+
+    // add to page
+    city.append(cityDate, image)
+    cardBody.append(city, temperature, humidity, wind);
+    card.append(cardBody);
+    $("#currentCity").append(card)
+
+}
